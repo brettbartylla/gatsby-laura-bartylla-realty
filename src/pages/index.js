55ba4ab3a54dd38c2b7411a177e6a1ed03193e5f
brett <brett.bartylla@outlook.com>
2022-01-27 19:08:10 -0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+//images
+// import cardGif from '../images/cards.gif'
+
+
+//data pulls in query "data" from below
+const IndexPage = ({data}) => (
+  <Layout>
+    <Seo title="Home" />
+
+    <div className="Grid--landing">
+      <div></div>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
+
+export const query = graphql`
+  query {
+    cardGatsbyImage: file(relativePath: {eq: "laura_bartylla.webp"}) {
+      childImageSharp {
+        fluid(maxWidth: 2000) {
+        ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  },
+`
