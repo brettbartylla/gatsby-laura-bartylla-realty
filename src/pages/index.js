651ce15c02749d99c4162deff6a7162dfc9740ef
brett <brett.bartylla@outlook.com>
2022-01-27 20:29:20 -0600

    feat(grid): added centered two column layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,15 +12,31 @@ import Img from 'gatsby-image'
 const IndexPage = ({data}) => (
   <Layout>
     <Seo title="Home" />
-
-    <div className="Grid--landing">
-      <div>
-      <h1>Laura Bartylla</h1>
-      <Img 
-          fluid={data.lauraBartyllaImage.childImageSharp.fluid}
-          alt="laura bartylla"
-          className=""
-        />
+    <div class="u-posRelativ">
+      <div className="Grid--landing">
+      {/* col 1 */}
+      <div>        
+          <Img 
+              fluid={data.lauraBartyllaImage.childImageSharp.fluid}
+              alt="laura bartylla"
+              className=""
+            />
+        </div>
+        {/* col 2 */}
+        <div>
+          <h1>Laura Bartylla <span>Real Estate Agent</span></h1>
+          <p>Laura Bartylla active real estate agent currently helping interested buyers find the right homes. 
+          Currently active Madison Wisconsin and the surrounding area</p>
+          <h2>Contact</h2>
+            <ul>
+              <li>
+                <a href="tel:+7154193817">715.419.3817</a>
+              </li>
+              <li>
+                <a href="mailto:laurabartylla.firstweber.com">laurabartylla.firstweber.com</a>
+              </li>
+            </ul>
+        </div>
       </div>
     </div>
   </Layout>
